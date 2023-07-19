239b958bfa8cb96709982796205a2a33f071202e
Will Patterson <wlp223@aol.com>
2023-07-19 18:59:46 -0400

    spawning enemy

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    [SerializeField]
+    private float _nextFire = 0.0f;
+    [SerializeField]
+    private int _lives = 3;
+    [SerializeField]
+    private SpawnManager _spawnManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        if(_spawnManager == null)
+        {
+            Debug.Log("The Spawn Manager is NULL.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        //
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+        //
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // if(transform.position.y >= 0)
+        // {
+        //     transform.position = new Vector3(transform.position.x, 0, 0);
+        // }
+        // else if(transform.position.y <= -3.98f)
+        // {
+        //     transform.position = new Vector3(transform.position.x, -3.98f, 0);
+        // }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.98f, 0), 0);
+
+        if(transform.position.x >= 11.26f)
+        {
+            transform.position = new Vector3(-11.26f, transform.position.y, 0);
+        }
+        else if(transform.position.x <= -11.26f)
+        {
+            transform.position = new Vector3(11.26f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _nextFire = Time.time + _fireRate;
+        //Instantiates _laserprefab at position of player at default rotation
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        _lives --;
+        if(_lives == 0)
+        {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
+    }
+}
